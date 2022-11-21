f6d0d152f5467a200e4508998ebae94c15fdb41b
Marion Ott <63477915+marionott@users.noreply.github.com>
2022-11-21 16:53:27 +0100

    fix(scripts): update _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,50 +1,55 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript
-} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
+
 import Script from 'next/script'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
+        />
 
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
-            onLoad={() => {
-              //@ts-ignore
-              window.dataLayer = window.dataLayer || []
-              function gtag() {
-                //@ts-ignore
-                dataLayer.push(arguments)
-              }
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}');`
+          }}
+        />
+        <script
+          async
+          src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_CONTAINER_ID}`}
+        />
+        {/* <Script
+          async
+          onLoad={() => {
+            console.log('SCRIPT LOADING!!!')
+            //@ts-ignore
+            window.dataLayer = window.dataLayer || []
+            function gtag() {
               //@ts-ignore
-              gtag('js', new Date())
-              //@ts-ignore
-              gtag('config', process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID)
-            }}
-          />
-          <Script
-            src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_CONTAINER_ID}`}
-          />
-        </Head>
+              dataLayer.push(arguments)
+            }
 
-        <body className='preload'>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+            //@ts-ignore
+            gtag('js', new Date())
+            //@ts-ignore
+            gtag('config', process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID)
+          }}
+        />
+        <Script
+          src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_CONTAINER_ID}`}
+        /> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
