3e210e66ad7320087c62ab641923bfe89a6d6620
Marion Ott <63477915+marionott@users.noreply.github.com>
2022-11-21 19:50:45 +0100

    feat(cookie): add remove cookie button

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,14 @@
-import { Text } from '@vercel/examples-ui'
+import { Button } from '@vercel/examples-ui'
+import Cookies from 'js-cookie'
 import OptimizeLayout from '../components/GoogleOptimizeLayout'
+import { COOKIE_NAME } from '../lib/constants'
 
 export default function About() {
+  const removeCookie = () => {
+    Cookies.remove(COOKIE_NAME)
+    window.location.reload()
+  }
+
   return (
     <>
       <h2>About page</h2>
@@ -9,6 +16,9 @@ export default function About() {
         You&apos;re currently on <b>/about</b>
       </p>
       <p>This is the original about page</p>
+      <Button variant='secondary' onClick={removeCookie}>
+        Remove cookie & reload
+      </Button>
     </>
   )
 }
